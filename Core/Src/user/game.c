07c011a4007d3c817c3e237ebd9dfbfd8a6218c5
Ali Jafari <ali81jafari@outlook.com>
2025-01-24 06:30:20 +0330

    feat: updated health system so that if circle is missed health gets lower

--- Core/Src/user/game.c
+++ Core/Src/user/game.c
@@ -236,31 +236,51 @@ void game_generate_shape_right(Game* game) {
   }
 }
 
-void game_update_health(Game* game) {
-  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] == SHAPE_SQUARE) {
+void game_update_health_circle(Game* game) {
+  if (game->road[LCD_WIDTH - 1][!game->cars_position[DIRECTION_LEFT] + 2] ==
+      SHAPE_CIRCLE) {
     game->health--;
-    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] = SHAPE_EMPTY;
   }
-  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] == SHAPE_SQUARE) {
+  if (game->road[LCD_WIDTH - 1][!game->cars_position[DIRECTION_RIGHT]] ==
+      SHAPE_CIRCLE) {
     game->health--;
-    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] = SHAPE_EMPTY;
+  }
+}
+
+void game_update_health_square(Game* game) {
+  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] ==
+      SHAPE_SQUARE) {
+    game->health--;
+    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] =
+        SHAPE_EMPTY;
+  }
+  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] ==
+      SHAPE_SQUARE) {
+    game->health--;
+    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] =
+        SHAPE_EMPTY;
   }
 }
 
 void game_update_points(Game* game) {
-  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] == SHAPE_CIRCLE) {
+  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] ==
+      SHAPE_CIRCLE) {
     game->points++;
-    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] = SHAPE_EMPTY;
+    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_LEFT] + 2] =
+        SHAPE_EMPTY;
   }
-  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] == SHAPE_CIRCLE) {
+  if (game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] ==
+      SHAPE_CIRCLE) {
     game->points++;
-    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] = SHAPE_EMPTY;
+    game->road[LCD_WIDTH - 1][game->cars_position[DIRECTION_RIGHT]] =
+        SHAPE_EMPTY;
   }
 }
 
 void game_cars_forward(Game* game) {
+  game_update_health_circle(game);
   shift_road(game);
-  game_update_health(game);
+  game_update_health_square(game);
   game_update_points(game);
   game_generate_shape_left(game);
   game_generate_shape_right(game);
